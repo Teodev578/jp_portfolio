7ab3ca0e0375e6200398ece70336ca3ef22f43cd
Teodev578 <kfabient@gmail.com>
2026-04-25 11:27:07 +0000

    feat: integrate Tailwind CSS and configure custom Google Fonts for the project styling

--- jp_react_portfolio/tailwind.config.js
+++ jp_react_portfolio/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  important: true,
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // C'est cette ligne qui débloque tout !
+  ],
   theme: {
-    extend: {},
+    extend: {
+      // On connecte Tailwind à tes variables CSS existantes !
+      fontFamily: {
+        sans: ['var(--font-headline)', 'sans-serif'], // Ton Space Grotesk
+        mono: ['var(--font-body)', 'monospace'],      // Ton Space Mono
+        serif: ['Playfair Display', 'Times New Roman', 'serif'], // Police élégante pour le contraste "Magazine"
+      }
+    },
   },
   plugins: [],
 }
-
