18ca2ac333b0328eed22a58cad22eea297f59082
Teodev578 <kfabient@gmail.com>
2026-04-26 22:37:16 +0000

    refactor: clean up unused project styles and remove deprecated ContactEmail component

--- jp_react_portfolio/src/components/sections/Contact.tsx
+++ jp_react_portfolio/src/components/sections/Contact.tsx
@@ -4,7 +4,6 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { SocialLink } from '../ui/SocialLink';
-import { ContactEmail } from '../ui/ContactEmail';
 
 gsap.registerPlugin(ScrollTrigger);
 
